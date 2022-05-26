ce3c04530c23532e14873cd2691d4dd2348667f8
lautixon <106138802+lautixon@users.noreply.github.com>
2022-05-26 14:43:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function myFunction() {
+  var x = document.getElementById("menudes");
+  if (x.style.display === "none") {
+      x.style.display = "flex";
+  } else {
+      x.style.display = "none";
+  }
+}
+
