1297642ff3b6953b644ea56b43f77fb46f1c3dbf
lautixon <106138802+lautixon@users.noreply.github.com>
2022-05-29 03:25:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+const boton = document.getElementById('nikemenusvg');
+const menudesplegable = document.getElementById('menudes');
+const botoncerrado = document.querySelector('.clickclose-icon');
+
+boton.addEventListener('click', () => {
+  menudesplegable.classList.toggle('activado');
+})
+botoncerrado.addEventListener('click', () => {
+  menudesplegable.classList.toggle('activado');
+})
+
+const botoncoleccion = document.querySelector('.a11');
+const coleccionmenu = document.querySelector('.coleccionesextendido');
 
 
-function myFunction() {
-  var x = document.getElementById("menudes");
-  if (x.style.display === "none") {
-      x.style.display = "flex";
-  } else {
-      x.style.display = "none";
-  }
-}
+
+botoncoleccion.addEventListener('click', () => {
+  coleccionmenu.classList.toggle('coleccionesextendidoactivo');
+  
+})
+
+
 
